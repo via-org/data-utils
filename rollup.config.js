af481c99e44e873195c7d3e4f1b9c407cd9e110d
Donald Geddes <hi@donaldgeddes.ca>
2022-02-13 17:29:02 -0800

    Specify external deps for rollup

--- rollup.config.js
+++ rollup.config.js
@@ -5,6 +5,7 @@ const input = './src/index.js'
 // eslint-disable-next-line import/no-default-export
 export default {
   input,
+  external: ['@hbauer/convenience-functions'],
   output: [
     { file: pkg.main, format: 'cjs' },
     { file: pkg.module, format: 'esm' },
