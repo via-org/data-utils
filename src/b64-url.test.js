9bbc341712c1b58ead7ef5051b2bde248ac4ab09
Donald Geddes <hi@donaldgeddes.ca>
2022-02-16 14:02:32 -0800

    Make tests more robust

--- src/b64-url.test.js
+++ src/b64-url.test.js
@@ -1,28 +1,24 @@
 import test from 'ava'
 
+import sampleUint8Array from '../test/sample-uint8array.js'
+import sampleB64Url from '../test/sample-b64url.js'
+
 import { base64url } from './b64-url.js'
 
 test('Should correctly stringify a Uint8Array-encoded value', t => {
-  const input = new Uint8Array([
-    84, 67, 51, 112, 211, 102, 37, 231, 177, 226, 71, 31, 111, 56, 106, 166, 55,
-    121, 206, 235, 57, 218, 173, 7, 178, 203, 82, 227, 38, 226, 101, 21,
-  ])
+  const input = sampleUint8Array
 
-  const expected = 'VEMzcNNmJeex4kcfbzhqpjd5zus52q0HsstS4ybiZRU'
   const actual = base64url.stringify(input)
+  const expected = sampleB64Url
 
   t.is(actual, expected)
 })
 
 test('Should correctly parse a Base64URL-encoded value', t => {
-  const input = 'VEMzcNNmJeex4kcfbzhqpjd5zus52q0HsstS4ybiZRU'
-
-  const expected = new Uint8Array([
-    84, 67, 51, 112, 211, 102, 37, 231, 177, 226, 71, 31, 111, 56, 106, 166, 55,
-    121, 206, 235, 57, 218, 173, 7, 178, 203, 82, 227, 38, 226, 101, 21,
-  ])
+  const input = sampleB64Url
 
   const actual = base64url.parse(input)
+  const expected = sampleUint8Array
 
   t.deepEqual(actual, expected)
 })
